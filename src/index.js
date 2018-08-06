2e866ef900e059a8f5c574f711e69dcda90a56f8
Derek Miranda <derek.miranda@ff0000.com>
2018-08-06 11:09:29 -0700

    Draw webcam stream to canvas

--- src/index.js
+++ src/index.js
@@ -8,18 +8,51 @@
 
   function redraw() {
     requestAnimationFrame(() => {
-      ctx.drawImage(video, 0, 0);
-      !video.paused && window.requestAnimationFrame(redraw);
+      if (video.HAVE_ENOUGH_DATA && !video.paused) {
+        ctx.drawImage(video, 0, 0);
+      }
+      window.requestAnimationFrame(redraw);
     });
   }
 
-  video.addEventListener('play', () => {
-    window.requestAnimationFrame(redraw);
-  });
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+  window.URL = window.URL || window.webkitURL;
+
+  if (!navigator.getUserMedia) {
+    window.alert('Sorry. navigator.getUserMedia() is not available.');
+  } else {
+    navigator.getUserMedia({ video: true }, gotStream, noStream);
+  }
+
+  function gotStream(stream) {
+    if (window.URL) {
+      video.src = window.URL.createObjectURL(stream);
+    } // Opera
+    else {
+      video.src = stream;
+    }
+
+    video.play();
+    redraw();
+
+    video.onerror = function(e) {
+      stream.stop();
+    };
+    stream.onended = noStream;
+  }
+
+  function noStream(e) {
+    var msg = 'No camera available.';
+    if (e.code == 1) {
+      msg = 'User denied access to use camera.';
+    }
+    window.alert(msg);
+  }
 
   canvas.addEventListener('click', () => {
     video.paused ? video.play() : video.pause();
   });
-
-  // setTimeout(() => video.play(), 0);
 })();
