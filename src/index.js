6b0acf0594c36506f893200a18f8d95b1e03272b
Derek Miranda <derek.miranda@ff0000.com>
2018-08-06 10:13:31 -0700

    WDS set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { butt } from "./functions";
+document.addEventListener('DOMContentLoaded', () => {
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 
-butt();
+  video.addEventListener('playing', () => {
+    console.log('playing');
+    ctx.drawImage(video, 0, 0);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, "public/dist"),
-    filename: "main.js"
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/'
   }
 };
