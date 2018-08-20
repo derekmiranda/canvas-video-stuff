728b208ea635338c464c7e9cf2833f3dde7631ee
Derek Miranda <derek.miranda.1992@gmail.com>
2018-08-20 11:44:12 -0400

    Html webpack plugin to generate html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-const path = require('path');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    publicPath: '/dist/'
-  }
+    contentBase: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'static/index.html'),
+      width: 500,
+      height: 500,
+    })
+  ]
 };
