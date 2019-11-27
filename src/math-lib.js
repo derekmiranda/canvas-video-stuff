59d8baf7da2f6725b3b0fbc7f7d87ef38cddd008
derekmiranda <derek.miranda.1992@gmail.com>
2019-11-27 11:38:27 -0800

    Trying testing...

--- src/math-lib.js
+++ src/math-lib.js
@@ -5,34 +5,44 @@ export const createSphereMappingFunc = ({
   angleMin,
   angleMax
 }) => point => {
-  return ((point - lenMin) / (lenMax - lenMin)) * (angleMax - angleMin) + angleMin;
-}
+  return (
+    ((point - lenMin) / (lenMax - lenMin)) * (angleMax - angleMin) + angleMin
+  );
+};
 
 export function mapPlaneToSphere({ i, j, width, height }) {
-  const radius = Math.floor(height / 2)
-  const midPt = Math.floor(width / 2)
+  const radius = Math.floor(height / 2);
+  const midPt = Math.floor(width / 2);
 
   // get angles to represent point on sphere
   const mapIToPhi = createSphereMappingFunc({
     lenMin: 0,
     lenMax: width - 1,
     angleMin: -Math.PI / 2,
-    angleMax: Math.PI / 2,
-  })
+    angleMax: Math.PI / 2
+  });
   const mapJToTheta = createSphereMappingFunc({
     lenMin: 0,
     lenMax: height - 1,
     angleMin: -Math.PI / 2,
-    angleMax: Math.PI / 2,
-  })
-  const phi = mapIToPhi(i)
-  const theta = mapJToTheta(j)
+    angleMax: Math.PI / 2
+  });
 
-  const x = Math.floor(radius * Math.sin(phi) * Math.cos(theta))
-  const y = Math.floor(radius * Math.cos(phi) * Math.sin(theta))
+  const phi = mapIToPhi(i);
+  const theta = mapJToTheta(j);
 
-  const xCorr = x + midPt
-  const yCorr = y + midPt
+  const x = Math.floor(radius * Math.sin(phi) * Math.cos(theta));
+  const y = Math.floor(radius * Math.cos(phi) * Math.sin(theta));
 
-  return { x: xCorr, y: yCorr, theta, phi }
+  const xCorr = x + midPt;
+  const yCorr = y + midPt;
+
+  if (i === 0 && j === 0) {
+    console.log('phi', phi);
+    console.log('theta', theta);
+    console.log('x', x);
+    console.log('y', y);
+  }
+
+  return { x: xCorr, y: yCorr, theta, phi };
 }
